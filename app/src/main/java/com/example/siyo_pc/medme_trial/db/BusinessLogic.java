a3e095cba45fb57cbfb64e3b8b5034c94d788174
ssim7496 <mrsiyosimayi@gmail.com>
2015-08-20 02:43:21 +0200

    Business Logic added

---
+++ app/src/main/java/com/example/siyo_pc/medme_trial/db/BusinessLogic.java
@@ -0,0 +1,176 @@
+package com.example.siyo_pc.medme_trial.db;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.siyo_pc.medme_trial.classes.MM_Person;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessLogic {
+    Context context;
+    private List<NameValuePair> params = new ArrayList<NameValuePair>();
+    public JSON_Handler jsonHandler = new JSON_Handler();
+    private List<JSONObject> objectList = new ArrayList<JSONObject> ();
+    private String urlGetAllPeople = "http://ssimayi-medme.co.za/test.php";
+    private String urlAddPerson = "http://www.ssimayi-medme.co.za/insertPerson.php";
+    private String urlTestFetch = "http://ssimayi-medme.co.za/testFetch.php";
+
+    public BusinessLogic(Context context) {
+        this.context = context;
+    }
+
+    /*public ArrayList<MM_Person> getPeopleList() {
+        ArrayList<MM_Person> peopleList = new ArrayList<MM_Person>();
+
+        params.add(new BasicNameValuePair("", ""));
+
+
+        try {
+            //String hi = dataAccess.execute(new String[]{urlGetAllPeople}).get();
+            //new DataAccessLayer(urlGetAllPeople, params).execute("");
+            String x = "Hi";
+        } catch (Exception e) {
+
+        }
+        return  peopleList;
+    }*/
+    public void getPeopleList() {
+        DataAccessLayerRetrieval dataRetrieval = new DataAccessLayerRetrieval(urlTestFetch);
+        dataRetrieval.execute();
+
+        List<MM_Person> personList = new ArrayList<MM_Person>();
+        /*for (JSONObject json : objectList) {
+            MM_Person person = new MM_Person();
+            String id = json.getString("A");
+        }*/
+
+       /* for (int i = 0; i < objectList.size(); i++) {
+            //JSON conversion
+        }*/
+    }
+
+    public void addPerson(MM_Person person) {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+        nameValuePairs.add(new BasicNameValuePair("personName", person.GetPersonName()));
+        nameValuePairs.add(new BasicNameValuePair("personSurname", person.GetPersonSurname()));
+        nameValuePairs.add(new BasicNameValuePair("personEmailAddress", person.GetPersonEmailAddress()));
+        nameValuePairs.add(new BasicNameValuePair("personCellNumber", person.GetPersonCellphoneNumber()));
+        nameValuePairs.add(new BasicNameValuePair("personPassword", person.GetPersonPassword()));
+        nameValuePairs.add(new BasicNameValuePair("personRecoveryQuestion", person.GetPersonRecoveryQuestion()));
+        nameValuePairs.add(new BasicNameValuePair("personRecoveryAnswer", person.GetPersonRecoveryAnswer()));
+
+        DataAccessLayerOperational dataAccess = new DataAccessLayerOperational(urlAddPerson, nameValuePairs);
+        dataAccess.execute();
+    }
+
+    /*public MM_Person getPerson() {
+        MM_Person person = new MM_Person();
+
+        return  person;
+    }*/
+
+    class DataAccessLayerOperational extends AsyncTask<String, Void, String> {
+        private ProgressDialog progressDialog = new ProgressDialog(context);
+        private List<NameValuePair> params = new ArrayList<NameValuePair>();
+        private String url;
+        private String message;
+
+        public DataAccessLayerOperational(String url, List<NameValuePair> params) {
+            this.url = url;
+            this.params = params;
+        }
+        @Override
+        protected void onPreExecute() {
+            progressDialog.setMessage("Processing ...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... param) {
+            try {
+                String result = jsonHandler.getJSONFromUrl(url, params);
+
+                JSONObject jsonResponse = new JSONObject((result));
+                JSONArray jArray = jsonResponse.getJSONArray("final");
+                JSONObject jsonMessage = jArray.getJSONObject(0);
+                message = jsonMessage.getString("message");
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in parsing data ");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String file_url)
+        {
+            progressDialog.dismiss();
+            Toast.makeText(context.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+        }
+
+    }
+
+    class DataAccessLayerRetrieval extends AsyncTask<String, Void, List<JSONObject>> {
+
+        private ProgressDialog progressDialog = new ProgressDialog((context));
+
+        private String url;
+
+        public DataAccessLayerRetrieval(String url) {
+            this.url = url;
+        }
+
+        protected void onPreExecute() {
+            progressDialog.setMessage("Fetching information ...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected List<JSONObject> doInBackground(String... param) {
+            try {
+                String result = jsonHandler.getJSONFromUrl(url);
+
+                JSONObject jsonResponse = new JSONObject((result));
+                JSONArray jArray = jsonResponse.getJSONArray("finalFetch");
+
+                for (int i = 0; i < jArray.length(); i++) {
+                    /*JSONObject jObject = null;
+                    jObject = jArray.getJSONObject(i);
+
+                    String id = jObject.getString("PersonID");
+                    String name = jObject.getString("PersonName");
+                    String desc = jObject.getString("PersonSurname");*/
+                    JSONObject jsonObject = jArray.getJSONObject(i);
+                    objectList.add(jsonObject);
+                }
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in parsing data ");
+            }
+            return objectList;
+        }
+
+        protected void onPostExecute(String file_url) {
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+
+        }
+
+    }
+}
